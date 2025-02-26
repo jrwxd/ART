62871548a30c9a3ddb7ab14284e4c4f6e225b583
jrwxd <140230372+jrwxd@users.noreply.github.com>
2025-02-25 20:39:14 -0600

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -5,7 +5,11 @@ document.addEventListener('DOMContentLoaded', function() {
         'images/image2.png',
         'images/image3.png',
         'images/image4.png',
-        'images/image5.png'
+        'images/image5.png',
+        'images/image6.png',
+        'images/image7.png',
+        'images/image8.png',
+        'images/image9.png'
     ];
     
     let currentIndex = 0;
